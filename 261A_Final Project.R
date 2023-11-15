c880d9ee1a272cda6b6606b8383757717114fcb3
VerityZhong <150777547+VerityZhong@users.noreply.github.com>
2023-11-14 22:32:32 -0800

    261A_Final Project.R

--- 261A_Final Project.R
+++ 261A_Final Project.R
@@ -1,13 +1,7 @@
 library(openintro)
 loans <- loans_full_schema
 
-
 project <- subset.data.frame(loans, select = c(interest_rate, grade, annual_income, total_credit_lines, num_historical_failed_to_pay, total_credit_limit, debt_to_income, installment, loan_purpose, term, application_type, homeownership, loan_amount, public_record_bankrupt, total_credit_utilized, num_total_cc_accounts))
-
-# Find missing elements using is.na()
-missing_elements <- which(is.na(project), arr.ind = TRUE)
-print(missing_elements)
-project <- na.omit(project)
 
 #fit the full model
 fit <- lm(interest_rate ~ grade + annual_income + total_credit_lines + 
@@ -16,47 +10,18 @@ fit <- lm(interest_rate ~ grade + annual_income + total_credit_lines +
             homeownership + loan_amount + public_record_bankrupt + 
             total_credit_utilized + num_total_cc_accounts, 
           data = project)
-summary(fit)
-plot(project)
 
-# Assuming lm_model is your linear regression model
-residuals <- residuals(fit)
+fit_debt_to_income <- lm(interest_rate ~ debt_to_income, data = project)
+plot(fit_debt_to_income)
 
-# Get the names of quantitative predictors in your model
-quantitative_predictors <- c("interest_rate", "annual_income", "total_credit_lines", "total_credit_limit", "debt_to_income", "installment", "loan_amount", "total_credit_utilized", "num_total_cc_accounts")
+fit_installment <- lm(interest_rate ~ installment, data = project)
+plot(fit_installment)
 
-# Set up a multi-panel plot
-par(mfrow = c(2, length(quantitative_predictors)))
+fit_loan_amount <- lm(interest_rate ~ loan_amount, data = project)
+plot(fit_loan_amount)
 
-# Loop through each quantitative predictor
-for (predictor in quantitative_predictors) {
-  # Extract the predictor values and residuals
-  predictor_values <- project[[predictor]]
-  residuals <- residuals(fit)
-  
-  # Check lengths
-  if (length(predictor_values) == length(residuals)) {
-    # Set up a multi-panel plot
-    par(mfrow = c(1, 2))
-    
-    # Residual plot
-    plot(predictor_values, residuals,
-         main = paste("Residuals vs.", predictor),
-         xlab = predictor, ylab = "Residuals", pch = 20)
-    abline(0,0)
-    
-    # QQ plot
-    qqnorm(residuals)
-    qqline(residuals)
-    
-    # Reset the plotting layout
-    par(mfrow = c(1, 1))
-    
-    # Pause to view each plot, press Enter to continue
-    cat("Press Enter to continue...")
-    readline()
-  } else {
-    warning("Length mismatch for", predictor)
-  }
-}
+fit_total_credit_utilized <- lm(interest_rate ~ total_credit_utilized, data = project)
+plot(fit_total_credit_utilized)
 
+fit_num_total_cc_accounts <- lm(interest_rate ~ num_total_cc_accounts, data = project)
+plot(fit_num_total_cc_accounts)
